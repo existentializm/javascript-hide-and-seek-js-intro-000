a613dd2d842dd83917e7dbb6499878a1160ed6ea
Liz Jaluague <e.jaluague@gmail.com>
2020-07-30 02:52:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function getFirstSelector(selector) {
+  return document.selector;
+}
 
+function nestedTarget() {
+  return document.getElementById('nested');
+}
+
+function increaseRankBy(n) {
+  return document.getElementById('nested');
+}
+
+function deepestChild() {
+  return document.getElementById('nested');
+}
